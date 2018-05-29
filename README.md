ca3f68c2cbc688b206851721dfec649e91aba134
desanni <dacen.c.waters@gmail.com>
2018-05-29 10:50:48 -0400

    trying to setup for pip installer

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-stmspectra
+# STMspectra
+
+Information available on https://github.com/desanni/stmspectra
 
 Plotting tool for STMAFM 3.1 data. Inputs .VERT files to conveniently compare spectra.
 
 Can show details for spectra such as modulation voltage, voltage and z ramps, and set point current.
 
 Can perform some simple analysis: normalize based on z ramp and average forward/backward sweeps implementing a hysteresis correction.
+
--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='stmspectra',
     version='0.1',
@@ -7,8 +10,15 @@ setup(
     maintainer='Dacen Waters',
     maintainer_email='dacen.c.waters@gmail.com',
     description='Spectra plotting tool for STM',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     install_requires=['numpy', 'matplotlib'],
     url='https://github.com/desanni/stmspectra',
-    scripts=['bin/stmpspectra.py', 'bin/plot_spectra.py', 'bin/parameters.py', 'bin/spectra_analysis.py']
+    scripts=['bin/stmpspectra.py', 'bin/plot_spectra.py', 'bin/parameters.py', 'bin/spectra_analysis.py'],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "License :: GNU GPLv3",
+        "Operating System :: OS Independent",
+    ),
 )
     
